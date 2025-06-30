1f7c7543a2726e9ed7ef5f0294cbd65bfcc1ef80
LBROCHARD <lewis.brochard@gmail.com>
2025-06-30 15:13:44 +0200

    feat: added the ADIalogAddThought component

--- src/components/DialogAddThought/DialogAddThought.tsx
+++ src/components/DialogAddThought/DialogAddThought.tsx
@@ -1,5 +1,33 @@
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
+import { Button } from "../ui/button";
+import { Textarea } from "../ui/textarea";
+
 const DialogAddThought = () => {
-	return <div>DialogAddThought</div>;
+	return (
+		<>
+			<Dialog>
+				<DialogTrigger>Open Dialog</DialogTrigger>
+				<DialogContent>
+					<DialogHeader>
+						<DialogTitle>Ajouter une pensée</DialogTitle>
+						<DialogDescription>Quelle est ta pensée du jour ?</DialogDescription>
+						<Textarea/>
+					</DialogHeader>
+					<DialogFooter>
+						<Button type="submit">Envoyer</Button>
+					</DialogFooter>
+				</DialogContent>
+			</Dialog>
+		</>
+	)
 };
 
 export default DialogAddThought;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,10 @@
+import DialogAddThought from "@/components/DialogAddThought/DialogAddThought";
+
 const Home = () => {
 	return (
 		<div>
 			<h1>Home</h1>
+			<DialogAddThought/>
 		</div>
 	);
 };
