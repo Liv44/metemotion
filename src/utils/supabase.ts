e130a063a9b6f93d87f09ec15a2b7963b6011b21
Olivia Moreau <moreauolivia49@gmail.com>
2025-07-01 13:37:34 +0200

    feat(colors): add useGetColors

--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -4,6 +4,4 @@ import type { Database } from "./database.types";
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL as string;
 const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY as string;
 
-console.log(supabaseUrl, supabaseKey);
-
 export const supabase = createClient<Database>(supabaseUrl, supabaseKey);
