dfe25b56274b9467527bfca8b5c14231463678de
Mattéo Grellier <matteogrellier@gmail.com>
2025-07-07 16:21:45 +0200

    feat(share): add colors choice

--- src/pages/SharePage.tsx
+++ src/pages/SharePage.tsx
@@ -1,3 +1,4 @@
+import ColorBlock from "@/components/ColorBlock";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
@@ -8,7 +9,9 @@ import {
 	SelectTrigger,
 	SelectValue,
 } from "@/components/ui/select";
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
 import { useState } from "react";
+import useColors from "@/usecases/useGetColors";
 
 type Humor = "JOIE" | "TRISTESSE" | "COLÈRE" | "PEUR" | "SURPRISE";
 
@@ -21,6 +24,7 @@ const SharePage = () => {
 		"SURPRISE",
 	];
 	const [humor, setHumor] = useState<Humor | undefined>(undefined);
+	const { colors } = useColors();
 
 	return (
 		<div className="flex flex-col items-center justify-center h-screen gap-4 p-4">
@@ -52,8 +56,22 @@ const SharePage = () => {
 					<Label htmlFor="mot-cle">Mot clé</Label>
 					<Input type="text" placeholder="Mot clé" id="mot-cle" />
 				</div>
+				<div className="flex flex-row gap-x-4 items-center justify-center display">
+					<RadioGroup defaultValue="bleu">
+						{colors?.map(color => (
+							<div
+								key={color.id}
+								className="flex flex-row gap-x-2 items-center"
+							>
+								<RadioGroupItem value={color.id} />
+								<ColorBlock color={color.hex || ""} />
+								<Label htmlFor={color.id}>{color.name}</Label>
+							</div>
+						))}
+					</RadioGroup>
+				</div>
 				<Button variant="outline" role="button" type="submit">
-					Click me
+					Créer une émotion
 				</Button>
 			</form>
 		</div>
